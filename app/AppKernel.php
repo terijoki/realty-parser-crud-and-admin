f9868397b5a6c03b3e48ed9218ad512c10147bee
terijoki <terijoki@list.ru>
2020-05-17 22:06:22 +0300

    First page with easy_admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,8 @@ class AppKernel extends Kernel
             new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
             new RltBundle\RltBundle(),
         ];
 
@@ -36,6 +38,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             $bundles[] = new Symfony\Bundle\MakerBundle\MakerBundle();
+            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             if (self::ENV_TEST === $this->getEnvironment()) {
                 $bundles[] = new Liip\FunctionalTestBundle\LiipFunctionalTestBundle();
             }
